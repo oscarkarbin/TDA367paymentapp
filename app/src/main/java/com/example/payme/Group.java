10a1fd997d566dca1cbc81b3e9f9b9698a414806
antonboras <boras@net.chalmers.se>
2022-09-13 09:47:57 +0200

    Update Group.java
    Påbörjade Group.java

--- app/src/main/java/com/example/payme/Group.java
+++ app/src/main/java/com/example/payme/Group.java
@@ -6,6 +6,11 @@ import java.util.List;
 public class Group {
     private List<Member> groupMembers = new ArrayList<>();
     private List<Event> groupEvents = new ArrayList<>();
+    private String name;
 
 
+    public Group(String name, List<Member> members) {
+        this.name = name;
+        this.groupMembers = members;
+    }
 }
