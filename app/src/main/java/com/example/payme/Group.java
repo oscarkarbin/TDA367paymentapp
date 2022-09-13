fa278f9b34b6a8de7dbfa4b1287e53b88ec4fd31
antonboras <boras@net.chalmers.se>
2022-09-13 09:44:24 +0200

    Påbörjat Group.java

--- app/src/main/java/com/example/payme/Group.java
+++ app/src/main/java/com/example/payme/Group.java
@@ -1,4 +1,11 @@
 package com.example.payme;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Group {
+    private List<Member> groupMembers = new ArrayList<>();
+    private List<Event> groupEvents = new ArrayList<>();
+
+
 }
