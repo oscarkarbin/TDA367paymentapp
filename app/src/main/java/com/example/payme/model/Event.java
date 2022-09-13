68220709c08867eccdf4f33d5fba2868113b2d0e
Oscar Karbin <karbin.oscar2002@gmail.com>
2022-09-13 11:27:42 +0200

    Added Event constructor

--- app/src/main/java/com/example/payme/model/Event.java
+++ app/src/main/java/com/example/payme/model/Event.java
@@ -1,4 +1,13 @@
 package com.example.payme.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Event {
+    private List<Member> eventMembers = new ArrayList<>();
+    private String eventName;
+
+    public Event(String eventName, List<Member> eventMembers){
+        this.eventName = eventName;
+    }
 }
