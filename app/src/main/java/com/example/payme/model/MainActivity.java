fb4e2bb677c3d207d53cbb1ce1c8a5f37c0a210e
jihad <jihad.mohammed95@gmail.com>
2022-09-14 10:20:42 +0200

    Gui

--- app/src/main/java/com/example/payme/model/MainActivity.java
+++ app/src/main/java/com/example/payme/model/MainActivity.java
@@ -2,15 +2,25 @@ package com.example.payme.model;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Dialog;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.payme.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    Dialog myDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDialog= new Dialog(this);
     }
-}
+
+    public void onClick(View view) {
+        myDialog.setContentView(R.layout.creategroup);
+        myDialog.show();
+
+        }
+    }
