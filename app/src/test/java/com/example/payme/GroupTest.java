491124d33e04ce05af92a3513426ce448741e724
antonboras <boras@net.chalmers.se>
2022-09-15 13:34:24 +0200

    Fixat lite group test

--- app/src/test/java/com/example/payme/GroupTest.java
+++ app/src/test/java/com/example/payme/GroupTest.java
@@ -14,7 +14,7 @@ import static org.junit.Assert.*;
 
 public class GroupTest {
     @Test
-    public void CreatGroup(){
+    public void testCreateGroup(){
         Member Anton = new Member("Anton", "076258015");
         Member Jihad = new Member("jihad", "072827822");
         Member Oskar = new Member("Oskar", "072827822");
@@ -27,7 +27,7 @@ public class GroupTest {
     }
 
     @Test
-    public void CreateEvent(){
+    public void testCreateEvent(){
         Member Anton = new Member("Anton", "076258015");
         Member Jihad = new Member("jihad", "072827822");
         Member Oskar = new Member("Oskar", "072827822");
@@ -35,8 +35,34 @@ public class GroupTest {
         list.add(Anton);
         list.add(Jihad);
         list.add(Oskar);
-        Event event = new Event("middag",list, new PaySplit());
-        event.markEventAsDone();
-        assertFalse(event.isActive());
+        Group g = new Group("grupp1", list);
+        g.createEvent("middag", new PaySplit());
+
+        assertEquals(g.getGroupEvents().size(), 1);
+    }
+    @Test
+    public void testRemoveMember() {
+        Member Anton = new Member("Anton", "076258015");
+        Member Jihad = new Member("jihad", "072827822");
+        Member Oskar = new Member("Oskar", "072827822");
+        List<Member> list = new ArrayList<Member>();
+        list.add(Anton);
+        list.add(Jihad);
+        list.add(Oskar);
+        Group g = new Group("grupp1", list);
+        g.removeMember(Anton);
+        assertFalse(g.getGroupMembers().contains(Anton));
+    }
+    @Test
+    public void testAddMember() {
+        Member Anton = new Member("Anton", "076258015");
+        Member Jihad = new Member("jihad", "072827822");
+
+        List<Member> list = new ArrayList<Member>();
+        list.add(Anton);
+        list.add(Jihad);
+        Group g = new Group("grupp1", list);
+        g.addNewMember("Oskar", "072827822");
+        assertEquals(g.getGroupMembers().size(), 3);
     }
 }
