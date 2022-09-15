db51e363489cc3a6279970225e371ca9155beaab
jihad <jihad.mohammed95@gmail.com>
2022-09-15 13:52:41 +0200

    Tests

---
+++ app/src/test/java/com/example/payme/MemberTest.java
@@ -0,0 +1,14 @@
+package com.example.payme;
+
+import com.example.payme.model.Member;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class MemberTest {
+    @Test
+    void createMember(){
+        Member Jihad = new Member("jihad", "072827822");
+        //assertEquals();
+    }
+}
